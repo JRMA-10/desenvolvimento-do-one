703a62dcef9ad9b61a1bb7aee4e8055fe5721b9c
PARZIVAL <mendoncaalexandrejoaorafael@gmail.com>
2026-01-21 21:18:49 -0300

    Adicionando telas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame as pg
-from random import randint, choice, shuffle
-import time
+from random import randint, shuffle, choice
 
 # INICIALIZAÇÃO
 pg.init()
@@ -13,14 +12,11 @@ fonte_grande = pg.font.Font(None, 36)
 
 # CORES
 BRANCO = (255, 255, 255)
-AMARELO = (255, 255, 0)
-VERDE = (20, 120, 20)
 PRETO = (0, 0, 0)
 
-#Contadores 
-tempo_inicio_jogo = pg.time.get_ticks()
-tempo_inicio_turno = pg.time.get_ticks()
+#Contadores
 tempo_limite_turno = 10000
+tempo_inicio_turno = pg.time.get_ticks()
 
 # CARTAS
 def carregar_cartas():
@@ -72,33 +68,47 @@ def carregar_cartas():
 cartas = carregar_cartas()
 shuffle(cartas)
 
-
 #Bolo 
 bolo = []
 
+#Telas do jogo:
+MENU_INICIAL = 0
+JOGO = 1
+PAUSA = 2
+TELA_FINAL = 3
+ESTADO = MENU_INICIAL
+
+#Menu inical 
+def menu_inical():
+    global ESTADO
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_RETURN]:
+        ESTADO = JOGO
 
 #Funções principais
-def sorteando():
-    sorteio = randint(0, len(cartas)-1)
-    return sorteio
+def sorteando(cartas):
+    return randint(0, len(cartas)-1) 
 
 #cartas dos players
-def distribuindo(mao):
+def distribuindo(mao, cartas):
     for i in range(7):
-        num_carta_escolhida = sorteando()
+        num_carta_escolhida = sorteando(cartas)
         carta = cartas[num_carta_escolhida]
         cartas.remove(cartas[num_carta_escolhida])
         mao.append(carta)
     
 
-def possiveis_jogadas(mao):
+def puxar_cartas(mao):
     contador = 0
     for carta in mao:
         if carta['cor'] == ultima['cor'] or carta['numero'] == ultima['numero'] or carta['tipo'] in ['+4', '-4', 'comunista', 'tornado']:
             contador += 1
-    return contador
+    if contador == 0:
+        return True
+    return False
 
-def puxar_cartas_mais(lista_de_cartas):
+def puxar_cartas_mais(lista_de_cartas, cartas):
     if ultima['tipo'] == '+2':
         for i in range(2):
             lista_de_cartas.append(cartas[0])
@@ -115,42 +125,29 @@ def puxar_cartas_mais(lista_de_cartas):
         for i in range(4):
             cartas.append(lista_de_cartas[0])
             lista_de_cartas.pop(0)
-    
-def puxar_cartas(mao, cartas): 
-    num = possiveis_jogadas(mao)
-    if num == 0:
-        mao.append(cartas[0])
-        cartas.pop(0)
-    
-def jogadas_do_jogador(mao, bolo):
-    puxar_cartas(cartas_jogador, cartas) #Colocar isso no loop
-    carta_selecionada = 0
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_0:
-                carta_selecionada = 0
-            if event.key == pg.K_1:
-                carta_selecionada = 1
-            if event.key == pg.K_2:
-                carta_selecionada = 2
-            if event.key == pg.K_3:
-                carta_selecionada = 3
-            if event.key == pg.K_4:
-                carta_selecionada = 4
-            if event.key == pg.K_5:
-                carta_selecionada = 5
-            if event.key == pg.K_6:
-                carta_selecionada = 6
-            if event.key == pg.K_o:
-                print('Jogador só tem uma carta!')
-    
-    if ultima['cor'] == cartas_jogador[carta_selecionada]['cor'] or ultima['numero'] == cartas_jogador[carta_selecionada]['numero'] or cartas_jogador[carta_selecionada]['tipo'] in ['+4', '-4', 'comunista', 'tornado']:
-        print('Jogada válida!')
-        bolo.append(cartas_jogador[carta_selecionada])
-        mao.pop(carta_selecionada)
 
-def jogada_computador(mao, bolo):
-    puxar_cartas(mao, cartas)
+def carta_valida(carta, ultima):
+    return (carta["cor"] == ultima["cor"]
+        or carta["numero"] == ultima["numero"]
+        or carta["tipo"] == "+4"
+        or carta["tipo"] == "+10"
+        or carta["tipo"] == "-4"
+        or carta["tipo"] == "tornado"
+        or carta["tipo"] == "comunista"
+    )
+
+def funcao_tornado(mao1, mao2):
+    mao1[:], mao2[:] = mao2[:], mao1[:]
+
+carta_selecionada = 0
+def jogadas_do_jogador(mao, bolo, carta_selecionada):
+    if carta_valida(carta_selecionada, ultima) == True:
+        bolo.append(mao[carta_selecionada])
+        mao.pop(carta_selecionada)
+    else:
+        print("Jogada inválida!")
+
+def jogadas_computador(mao, bolo):
     possiveis_jogadas = []
     for carta in mao:
         if carta['cor'] == ultima['cor'] or carta['numero'] == ultima['numero'] or carta['tipo'] in ['+4', '-4', 'comunista', 'tornado']:
@@ -160,25 +157,47 @@ def jogada_computador(mao, bolo):
     bolo.append(jogada_final)
     mao.remove(jogada_final)
 
+def cartas_de_acao(mao):
+    global turno, tempo_inicio_turno
+    if ultima['tipo'] == 'bloqueio':
+        tempo_inicio_turno = pg.time.get_ticks()
+        if turno == 'jogador':
+            turno = 'computador'
+        else:
+            turno = 'jogador'
+    if puxar_cartas(mao) == True:
+        mao.append(cartas[0])
+        cartas.pop(0)
+        tempo_inicio_turno = pg.time.get_ticks()
+        if turno == 'jogador':
+            turno = 'computador'
+        else:
+            turno = 'jogador'
+    if ultima['tipo'] == 'tornado':
+        funcao_tornado(cartas_jogador, cartas_computador)
+        if turno == 'jogador':
+            turno = 'computador'
+        else:
+            turno = 'jogador'
+
 #carta inicial do bolo
-num_carta_inicial = sorteando()
+num_carta_inicial = sorteando(cartas)
 carta_inicial = cartas[num_carta_inicial]
 cartas.remove(cartas[num_carta_inicial])
 bolo.append(carta_inicial)
 
-
 #jogador
 cartas_jogador = []
 quantidade_cartas_jogador = len(cartas_jogador)
-distribuindo(cartas_jogador)
+distribuindo(cartas_jogador, cartas)
     
 #computador
 cartas_computador = []
 quantidade_cartas_computador = len(cartas_computador)
-distribuindo(cartas_computador)
+distribuindo(cartas_computador, cartas)
 
 #Turno
-turno = 'jogador'
+turno = choice(['jogador', 'computador'])
 
 while True:
     relogio.tick(60)
@@ -187,13 +206,24 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
     
+    if ESTADO == MENU_INICIAL:
+        menu_inicial()
+    
+    tempo_restante = max(0, (tempo_limite_turno - (pg.time.get_ticks() - tempo_inicio_turno)) // 1000)
+    tela.blit(
+        fonte.render(f"Turno: {turno.upper()} | Tempo: {tempo_restante}s", True, BRANCO),
+        (20, 20)
+    )
+    
     #Recuperando a última carta do bolo
     ultima = bolo[-1]
 
     mouse_pos = pg.mouse.get_pos()
     mouse_click = pg.mouse.get_pressed()[0]
 
-    # CARTAS DO JOGADOR
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    # APARIÇÃO DAS CARTAS DO JOGADOR    
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     largura = 100
     altura = 150
     espacamento = 20
@@ -215,19 +245,32 @@ while True:
         img = pg.transform.scale(carta["imagem"], (largura, altura))
         tela.blit(img, (x, y_anim))
 
+        if sobre_carta and mouse_click and turno == 'jogador':
+            carta_selecionada = i
+
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    # LÓGICA DO JOGADOR
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     if turno == 'jogador':
-        puxar_cartas_mais(cartas_jogador)
-        jogadas_do_jogador(cartas_jogador, bolo)
+        puxar_cartas_mais(cartas_jogador, cartas)
+        cartas_de_acao(cartas_jogador)
+        jogadas_do_jogador(cartas_jogador, bolo, carta_selecionada)
         fim = pg.time.get_ticks()
         if fim - tempo_inicio_turno >= tempo_limite_turno:
             print("Tempo acabou!")
             turno = 'computador'
             tempo_inicio_turno = pg.time.get_ticks()
+        carta_selecionada = None
+
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    # LÓGICA DO COMPUTADOR
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
     if turno == 'computador':
-        puxar_cartas_mais(cartas_computador)
+        puxar_cartas_mais(cartas_computador, cartas)
+        cartas_de_acao(cartas_computador)
         pg.time.delay(500)
-        jogada_computador(cartas_computador, bolo)
+        jogadas_computador(cartas_computador, bolo)
         turno = 'jogador'
     
     #transformando as cartas
